5f793247f512ce59e67774157abb6c376ad49756
kirkyky123 <131261278+kirkyky123@users.noreply.github.com>
2024-02-05 03:18:49 -0500

    done done

---
+++ projects/part3/All Time Stats/src.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById("menuToggle");
+const hamburgerDropdown = document.getElementById("hamburgerDropdown");
+
+menuToggle.onclick = function () {
+  hamburgerDropdown.classList.toggle("show");
+};
