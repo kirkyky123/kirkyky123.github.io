112d6799b2890dc0a08fa74a678fa81940fccdcf
kirkyky123 <131261278+kirkyky123@users.noreply.github.com>
2024-03-18 03:51:53 -0400

    forgot to add

--- projects/part5/home/src.js
+++ projects/part5/home/src.js
@@ -4,3 +4,12 @@ const hamburgerDropdown = document.getElementById("hamburgerDropdown");
 menuToggle.onclick = function () {
   hamburgerDropdown.classList.toggle("show");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const playerNameElement = document.getElementById("playerName");
+  const currentPlayerData = JSON.parse(sessionStorage.getItem("currentPlayer"));
+
+  if (currentPlayerData) {
+    playerNameElement.textContent = currentPlayerData.name;
+  }
+});
